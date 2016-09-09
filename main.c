22bc1808d0d94b14236183edd4bcd6d598c02745
mattski47 <matthewsski@gmail.com>
2016-09-08 21:17:43 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#define CHANNEL_SIZE 255;
+#define CHANNEL_SIZE 255
 
 typedef struct {
     unsigned char r;
@@ -15,11 +15,23 @@ typedef struct {
     unsigned char b;
 } Pixel;
 
-char format[2];
-int height;
-int width;
-int maxcolor;
-char* source = "imageP3.ppm";
+char out_format = '3';
+
+FILE* sourcefp;
+FILE* outputfp;
+char format[8];
+int h;
+int w;
+int mc;
+char height[8];
+char width[8];
+char maxcolor[8];
+char* source = "imageP6.ppm";
+unsigned char* data;
+
+void write_data_to_buffer();
+void output_p3();
+void output_p6();
 
 /*
  * 
@@ -31,7 +43,7 @@ int main(int argc, char** argv) {
         return(1);
     }
     
-    FILE* sourcefp = fopen(source, "r");
+    sourcefp = fopen(source, "r");
     
     if (!sourcefp) {
         fprintf(stderr, "Error: File not found.");
@@ -44,7 +56,71 @@ int main(int argc, char** argv) {
         return(1);
     }
     
+    fscanf(sourcefp, "%s %s", height, width);
+    
+    h = atoi(height);
+    w = atoi(width);
+    
+    if (h < 1 || w < 1) {
+        fprintf(stderr, "Error: Invalid dimensions.");
+        return(1);
+    }
+    
+    fscanf(sourcefp, "%s", maxcolor);
+    
+    mc = atoi(maxcolor);
+    
+    if (mc != CHANNEL_SIZE) {
+        fprintf(stderr, "Error: Channel size must be 8 bits.");
+        return(1);
+    }
+    
+    printf("%s\n", format);
+    printf("%d %d\n", h, w);
+    printf("%s", maxcolor);
+    
+    write_data_to_buffer(h, w);
+    fclose(sourcefp);
+    
+    outputfp = fopen("output.ppm", "w");
+    if (out_format == '3')
+        output_p3();
+    else
+        output_p6();
+    
+    fclose(outputfp);
     
     return(0);
 }
 
+void write_data_to_buffer() {
+    
+    data = malloc(sizeof(Pixel)*h*w);
+    
+    if (format[1] == '3') {
+        char temp[3];
+
+        for(int i=0; i<(sizeof(Pixel)*h*w); i++) {
+            fscanf(sourcefp, "%s", temp);
+            data[i] = (char)atoi(temp);
+        }
+    } else {
+        fscanf(sourcefp, "%s", data);
+    }
+    
+}
+
+void output_p3() {
+    fprintf(output, "P3\n");
+    fprintf(output, "%s %s\n", height, width);
+    fprintf(output, "%s\n", maxcolor);
+    for(int i=0; i<(sizeof(Pixel)*h*w); i++)
+        fprintf(output, "%d\n", data[i]);
+}
+
+void output_p6() {
+    fprintf(output, "P6\n");
+    fprintf(output, "%s %s\n", height, width);
+    fprintf(output, "%s\n", maxcolor);
+    fwrite(data, sizeof(Pixel), w*h, output);
+}
