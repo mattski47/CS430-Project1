94a3a9d28244188e81278e8f5d99ee5bd5e7e675
mattski47 <matthewsski@gmail.com>
2016-09-08 19:20:10 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -19,6 +19,7 @@ char format[2];
 int height;
 int width;
 int maxcolor;
+char* source = "imageP3.ppm";
 
 /*
  * 
@@ -26,12 +27,22 @@ int maxcolor;
 int main(int argc, char** argv) {
 
     if (argc != 4) {
-        fprintf(stderr, "Error: Arguments should be in format: format source dest");
+        fprintf(stderr, "Error: Arguments should be in format: 'format' 'source' 'dest'.");
         return(1);
     }
     
-    FILE *sourcefp = fopen("imageP3.ppm", "r");
+    FILE* sourcefp = fopen(source, "r");
     
+    if (!sourcefp) {
+        fprintf(stderr, "Error: File not found.");
+        return(1);
+    }
+    
+    fscanf(sourcefp, "%s", format);
+    if (format[0] != 'P' || (format[1] != '3' && format[1] != '6')) {
+        fprintf(stderr, "Error: Invalid image format. '%s' needs to be either 'P3' or 'P6'.", source);
+        return(1);
+    }
     
     
     return(0);
